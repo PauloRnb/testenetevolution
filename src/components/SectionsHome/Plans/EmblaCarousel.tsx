19a25b84c7ce98b869227dce3a2b9d1b34bf9d51
Paulo Rnb <paulornb99@gmail.com>
2025-07-23 14:46:02 -0300

    atualização do projeto

--- src/components/SectionsHome/Plans/EmblaCarousel.tsx
+++ src/components/SectionsHome/Plans/EmblaCarousel.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
 import { EmblaOptionsType } from "embla-carousel";
 import { DotButton, useDotButton } from "./EmblaCarouselDotButton";
 import {
