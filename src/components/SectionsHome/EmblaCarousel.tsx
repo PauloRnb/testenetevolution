26d8370dc6be70c0fe2407672081336b0adc21d2
Paulo Rnb <paulornb99@gmail.com>
2025-08-21 14:39:45 -0300

    Atualização do projeto

--- src/components/SectionsHome/EmblaCarousel.tsx
+++ src/components/SectionsHome/EmblaCarousel.tsx
@@ -109,7 +109,7 @@ export function EmblaCarousel() {
                   alt="Imagem mobile"
                   width={430}
                   height={242}
-                  priority
+                  fetchPriority="high"
                   className="block rounded-lg md:hidden"
                 />
               </div>
@@ -122,7 +122,7 @@ export function EmblaCarousel() {
                   alt="Card"
                   width={430}
                   height={139}
-                  priority
+                  fetchPriority="high"
                   className="w-full rounded-lg object-cover sm:w-[21rem]"
                 />
               </div>
