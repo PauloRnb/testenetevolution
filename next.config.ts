64151cb40fe52f54643df7e218c8a2b5bb2b9156
Paulo Rnb <paulornb99@gmail.com>
2025-08-22 17:41:38 -0300

    Att New 22-08-25

--- next.config.ts
+++ next.config.ts
@@ -14,15 +14,15 @@ const nextConfig: NextConfig = {
           { key: "Cross-Origin-Embedder-Policy", value: "require-corp" },
           {
             key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; " +
-              "img-src 'self' https: data:; " +
-              "script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://va.vercel-scripts.com 'strict-dynamic'; " +
-              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; " +
-              "font-src 'self' https://fonts.gstatic.com; " +
-              "connect-src 'self' https://www.google-analytics.com; " +
-              "frame-src 'self' https://www.youtube.com https://player.vimeo.com; " +
-              "require-trusted-types-for 'script';",
+            value: [
+              "default-src 'self';",
+              "img-src 'self' https: data:;",
+              "script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com https://va.vercel-scripts.com 'unsafe-inline';",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;",
+              "font-src 'self' https://fonts.gstatic.com;",
+              "connect-src 'self' https://www.google-analytics.com;",
+              "frame-src 'self' https://www.youtube.com https://player.vimeo.com;",
+            ].join(" "),
           },
         ],
       },
