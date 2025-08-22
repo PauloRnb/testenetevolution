b126974a8e3041886714aec2e0201c8a6d97012f
Paulo Rnb <paulornb99@gmail.com>
2025-08-21 23:29:16 -0300

    Atualização do projeto

--- src/components/SectionsHome/SlidersHome.tsx
+++ src/components/SectionsHome/SlidersHome.tsx
@@ -2,11 +2,10 @@
 
 import React from "react";
 import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
-import { Autoplay, Pagination, Navigation, EffectFade } from "swiper/modules";
+import { Autoplay, Pagination, EffectFade } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/effect-fade";
-import "swiper/css/navigation";
 
 import "@/app/styles.css";
 
